fde529246d065c1608a5a14ddd76b3fe3914f5d9
oljas medetbaev <medetbaevoljas@gmail.com>
2026-07-03 22:08:36 +0200

    BecomeTeacher component adjusted

--- src/components/authentication/become-teacher/BecomeTeacher.tsx
+++ src/components/authentication/become-teacher/BecomeTeacher.tsx
@@ -5,7 +5,7 @@ export default function BecomeTeacher() {
   const { profile, isAuthenticated } = useAuth();
   const { activateTeacher } = useBecomeTeacher();
   return (
-    <>
+    <div className="flex flex-col gap-2 text-center justify-center  mx-auto px-10 h-screen max-[350px]:px-4">
       {isAuthenticated ? (
         <button onClick={() => activateTeacher(profile?.id)}>
           Become a teacher
@@ -13,6 +13,6 @@ export default function BecomeTeacher() {
       ) : (
         <p>Please log in or sign up</p>
       )}
-    </>
+    </div>
   );
 }
