7f7b36907a27302b50ad6b386408e596ba794ef9
Oljas Medetbaev <medetbaevoljas@gmail.com>
2026-08-03 13:30:04 +0200

    Home link add to AdminNav

--- src/components/admin/ui/AdminNav.tsx
+++ src/components/admin/ui/AdminNav.tsx
@@ -1,9 +1,13 @@
 import { Link } from "react-router-dom";
 import MobileMenu from "./MobileMenu";
+import { HouseFill } from "react-bootstrap-icons";
 
 export default function AdminNav() {
   return (
-    <nav className="w-full max-[800px]:text-right">
+    <nav className="flex items-center px-10 max-[800px]:justify-between">
+      <Link to="/">
+        <HouseFill size={24} />
+      </Link>
       <ul className="flex gap-20 py-6 px-8 [&_a]:text-jet/50 [&_a]:text-2xl [&_a]:hover:text-jet max-[800px]:hidden">
         <li>
           <Link to="/admin/teachers">teachers</Link>
